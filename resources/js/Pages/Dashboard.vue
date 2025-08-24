e717cebb652081daa87507ef393d767f738cb924
Matheus Gomes <math.s.gomes@gmail.com>
2025-08-24 18:36:16 -0300

    add list of users dashboard

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,13 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import { Head } from '@inertiajs/vue3';
+
+const props = defineProps({
+    users: {
+        type: Array,
+        required: true
+    }
+});
 </script>
 
 <template>
@@ -8,20 +15,22 @@ import { Head } from '@inertiajs/vue3';
 
     <AuthenticatedLayout>
         <template #header>
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
-            >
+            <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
                 Dashboard
             </h2>
         </template>
 
         <div class="py-12">
             <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-                <div
-                    class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800"
-                >
+                <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg dark:bg-gray-800">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        You're logged in!
+                        <h3 class="mb-4 font-bold">Lista de Usuários</h3>
+
+                        <ul>
+                            <li v-for="user in props.users" :key="user.id" class="mb-2">
+                                {{ user.name }} ({{ user.email }})
+                            </li>
+                        </ul>
                     </div>
                 </div>
             </div>
